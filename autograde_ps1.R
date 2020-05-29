ab3a8f31e61ae6f0f1e64bda62665827dbacae7f
Jeremy Guillette <jguillette@fas.harvard.edu>
2020-05-29 14:18:06 -0400

    added explanatory comments

--- autograde_ps1.R
+++ autograde_ps1.R
@@ -12,7 +12,10 @@
 #
 # Author: Michael Guerzhoy guerzhoy@princeton.edu
 
-  
+
+# START FILE SETUP
+# This segment sets up the files needed for grading
+
 rm(list = ls())
 
 filename <- "ps1.R" # Assignment file name
@@ -29,6 +32,11 @@ if(assignment_title_env_var != ""){
   json.filename <- "results.json"
 }
 
+# END FILE SETUP
+
+# START ASSIGNMENT SETUP
+# This section is for things needed to assess the submitted file(s) that aren't the actual tests.
+# This is a good place to load sample data and set up structures needed for testing.
 
 suppressMessages(library(tidyverse))
 suppressMessages(library(jsonlite))
@@ -68,13 +76,24 @@ my.gap.2 <- tibble(country = c("countr1", "countr2", "countr1", "countr2"),
                    pop = c(30, 20, 32, 21),
                    gdpPercap =c(40, 50, 42, 52))
 
+# END ASSIGNMENT SETUP
+
+# START TEST SETUP
+# This section is where a list of tests is created. Each test in the list has several parts:
+# name: the name of the evaluation criterion shown to students
+# fun: the function to be run, as a text string
+# args: the arguments provided to that function, as a list of objects
+# expect: the expected output of `fun`, as an R object
+# visibility: whether this criterion will be shown to students. Accepted values are "visible" and "hidden"
+# weight: the number of points possible for this criterion
+
 test.cases <- list(
                   list(
                     name = "Continents sorted",
                     fun = "continents.gdp",
                     args = list(my.gap.1, 2),
                     expect = data.frame(continent = c("cont1", "cont2"), gdp = c(32*42, 21*52)),
-                    visibility = "visible", 
+                    visibility = "visible",
                     weight = 10
                   ),
                   list(
@@ -82,16 +101,22 @@ test.cases <- list(
                     fun = "continents.gdp",
                     args = list(my.gap.2, 2),
                     expect = data.frame(continent = c("cont1", "cont2"), gdp = c(21*52, 32*42)),
-                    visibility = "hidden", 
+                    visibility = "hidden",
                     weight = 10
                     )
           )
-                
+
+# END TEST SETUP
+
+# START RUN TESTS
+# The rest of the code runs the tests, then prepares the JSON output appropriately.
+# You probably don't want to edit it unless you have to.
+
 ret <- tryCatch(source(filename), error = function(c) c, warning = function(c) c ,message = function(c) c)
 if(!("visible" %in% names(ret))){
   tests <- list()
   tests[["tests"]] <- list()
-  
+
   for(i in 1:length(test.cases)){
     tests[["tests"]][[i]] <- list(test.cases[[i]][["name"]],
                                   score = 0,
@@ -99,8 +124,8 @@ if(!("visible" %in% names(ret))){
                                   visibility = test.cases[[i]][["visibility"]],
                                   output = my.toString(ret))
   }
-  
-  
+
+
 
   write(toJSON(tests, auto_unbox = T), file = json.filename)
   stop(my.toString(ret))
@@ -116,9 +141,9 @@ for(i in 1:length(test.cases)){
   cur.vis <- test.cases[[i]][["visibility"]]
   cur.weight <- test.cases[[i]][["weight"]]
   ret.val <- tryCatch(do.call(cur.fun, cur.args), error = function(c) c, warning = function(c) c ,message = function(c) c)
-  
+
   cur.output <- ""
-  
+
 
   if(my.toString(ret.val) == my.toString(cur.expect)){
       cur.score <-  cur.weight
@@ -130,9 +155,9 @@ for(i in 1:length(test.cases)){
   tests[["tests"]][[i]] <- list(name = cur.name,
                                 score = cur.score,
                                 max.score = cur.weight)
-  
+
   tests[["tests"]][[i]][["output"]] <- cur.output
-  
+
   if(cur.vis != "visible"){
     tests[["tests"]][[i]][["visibility"]] <- cur.vis
   }
@@ -147,16 +172,18 @@ cat.tests <- function(tests){
     cur.output <- tests[["tests"]][[i]][["output"]]
     score <- tests[["tests"]][[i]][["score"]]
     max.score <- tests[["tests"]][[i]][["max.score"]]
-    
-    
-    
+
+
+
     cat(sprintf("Test %s: %s(%s)\n", cur.name, cur.fun, my.toString(cur.args)))
     cat(sprintf("Expected: %s\n", my.toString(cur.expected)))
     cat(sprintf("Output:\n %s\n", my.toString(cur.output)))
     cat(sprintf("Score: %g/%g\n", score, max.score))
     cat("====================================================\n\n\n")
-                  
+
   }
 }
 cat.tests(tests)
 write(toJSON(tests, auto_unbox = T), file = json.filename)
+
+# END RUN TESTS
